b9a3ed622bc01203122d6b5db2472cbfdfeeec43
Mohammed Usama <usamabhakrani@gmail.com>
2024-01-29 14:14:01 +0500

    instance of buildAndPrintReport

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-import { MatchResult } from "./MatchResult";
 import { CsvFileReader } from "./CsvFileReaderBack";
 import { MatchReader } from "./MatchReader";
+import { Summary } from "./Summary";
+import { WinsAnalysis } from "./analysers/WinsAnalysis";
+import { ConsoleReport } from "./reportTargets/consoleReport";
 
-const csvFileReader = new CsvFileReader('football.csv')
-const matchReader = new MatchReader(csvFileReader)
-matchReader.load()
+const csvFileReader = new CsvFileReader("football.csv");
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
+const summary = new Summary(
+  new WinsAnalysis("Man United"),
+  new ConsoleReport()
+);
 
-
-
+summary.buildAndPrintReport(matchReader.matches);
